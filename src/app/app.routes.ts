6001e3fd7439fe810d29181609d456a1288e6ad0
anhtucao <caoanhtu611@gmail.com>
2024-01-20 19:08:02 +0700

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,15 @@ import { ProductsComponent } from './pages/admin/products/products.component';
 import { AdminComponent } from './layouts/admin/admin.component';
 import { CreateComponent } from './pages/admin/create/create.component';
 import { EditComponent } from './pages/admin/edit/edit.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { authGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: 'admin',
     component: AdminComponent,
@@ -18,5 +23,6 @@ export const routes: Routes = [
       { path: 'create', component: CreateComponent },
       { path: 'products/:id', component: EditComponent },
     ],
+    canActivate: [authGuard],
   },
 ];
